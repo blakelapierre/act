140eff2739734e0d81649d95a73e76eaf9800013
Blake La Pierre <blakelapierre@gmail.com>
2016-12-16 11:35:52 -0800

    name drop

--- src/vdom.js
+++ src/vdom.js
@@ -2,12 +2,10 @@ export default name =>
   code => {
     let root;
 
-    let i = (name, props, ...children) => (root = ({name, props, children}));
-
     if (name in global) throw Error(`${name} in global!`);
 
     //no support for 'dotted' names (nested namespaces)
-    global[name] = i;
+    global[name] = (name, props, ...children) => (root = ({name, props, children}));;
 
     const result = eval(code) || {}; // jshint ignore:line
 
