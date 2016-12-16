a0e3278e2c595b9c18ab83dfccbce8f72272a95f
Blake La Pierre <blakelapierre@gmail.com>
2016-12-16 11:37:02 -0800

    hold up on false evaluation; correction

--- src/vdom.js
+++ src/vdom.js
@@ -5,9 +5,9 @@ export default name =>
     if (name in global) throw Error(`${name} in global!`);
 
     //no support for 'dotted' names (nested namespaces)
-    global[name] = (name, props, ...children) => (root = ({name, props, children}));;
+    global[name] = (name, props, ...children) => (root = ({name, props, children}));
 
-    const result = eval(code) || {}; // jshint ignore:line
+    const result = eval(code) || new Error('hold up!'); // jshint ignore:line
 
     delete global[name];
 
